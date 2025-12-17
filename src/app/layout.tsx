75fdfd5641817cce62586bffb1613134393e28f2
chicoumbetov <shyngysmaaan@gmail.com>
2025-12-17 20:09:04 +0100

    improve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,13 @@ import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap", // * Prevents FOIT - Flash of Invisible Text
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
